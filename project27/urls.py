24a786403f673302167599a8b856a8ccfc27e2f2
Ritwik Sonam <99479173+RitwikSonam@users.noreply.github.com>
2025-09-26 21:19:06 +0530

    Update urls.py

--- project27/urls.py
+++ project27/urls.py
@@ -1,51 +1,35 @@
 """project27 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+ https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+1. Add an import:from my_app import views
+2. Add a URL to urlpatterns:path('', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+1. Add an import: from other_app.views import Home
+2. Add a URL to urlpatterns: path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+1. Import the include() function: from django.urls import include, path
+2. Add a URL to urlpatterns:path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from app.views import *
-from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home,name='home'),
-    path('get_weather/', views.get_weather, name='get_weather'),
+    path('home/',home,name='home'),
     path('registration/',registration,name='registration'),
     path('user_login/',user_login,name='user_login'),
     path('user_logout/',user_logout,name='user_logout'),
     path('profile_display/',profile_display,name='profile_display'),
     path('change_password/',change_password,name='change_password'),
     path('reset_password/',reset_password,name='reset_password'),
-    path('search/',search,name='search'),
-    path('user_history/',user_history,name='user_history'),
-    path('all_history/',all_history,name='all_history'),
-    path('edit-profile/', views.edit_profile, name='edit_profile'),
-    path('delete-profile/', views.delete_profile, name='delete_profile'),
+    
+    # NEW: Add a URL pattern for the weather search view
+    path('fetch-weather/', fetch_weather_data, name='fetch_weather_data'),
 
-
-
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-
-
-
-
-
-
-
-
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
